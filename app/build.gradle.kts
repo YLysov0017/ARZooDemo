a3b06ff9a29dd234f5b923d7c3903b8d94e3e149
YLysov0017 <yarlysov0017@gmail.com>
2023-10-15 17:49:39 +1000

    dd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("com.google.ar:core:1.40.0")
+    implementation("de.javagl:obj:0.4.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
